9acd509866969531a6b2417e0b83a369e0b3af2b
siluat <siluat@gmail.com>
2024-05-03 20:48:50 +0900

    🎨 (silu.at) html 태그 외 요소 테마 적용 방법 간소화

--- apps/silu.at/src/panda-config/custom-utils.ts
+++
@@ -1,4 +0,0 @@
-/**
- * body 태그 외 요소를 루트로 theme 적용할 때 사용
- */
-export const themeRootClassName = 'theme-root'
--- apps/silu.at/src/panda-config/global-styles.ts
+++ apps/silu.at/src/panda-config/global-styles.ts
@@ -1,12 +1,7 @@
 import { defineGlobalStyles } from '@pandacss/dev'
-import { themeRootClassName } from './custom-utils'
 
 export const globalStyles = defineGlobalStyles({
-  body: {
-    color: 'text',
-    bg: 'background',
-  },
-  [`.${themeRootClassName}`]: {
+  '[data-theme]': {
     color: 'text',
     bg: 'background',
   },
