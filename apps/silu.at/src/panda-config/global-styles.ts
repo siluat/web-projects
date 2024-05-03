bbe61b915e94d4e317f570ad69d11c16f8052cbd
siluat <siluat@gmail.com>
2024-05-03 23:56:16 +0900

    💄 (silu.at) Noto Sans KR을 기본 폰트로 설정

--- apps/silu.at/src/panda-config/global-styles.ts
+++ apps/silu.at/src/panda-config/global-styles.ts
@@ -1,6 +1,9 @@
 import { defineGlobalStyles } from '@pandacss/dev'
 
 export const globalStyles = defineGlobalStyles({
+  ':root': {
+    fontFamily: 'Noto Sans KR Variable, sans-serif',
+  },
   '[data-theme]': {
     color: 'text',
     bg: 'background',
