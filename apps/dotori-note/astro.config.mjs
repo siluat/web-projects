a146be3bd953c35e4111f77378544cbeef381fc1
siluat <siluat@gmail.com>
2024-08-24 19:45:13 +0900

    ✨ (dotori-note) 메인 페이지에 최근 변경 콘텐트 목록 표시

--- apps/dotori-note/astro.config.mjs
+++ apps/dotori-note/astro.config.mjs
@@ -1,9 +1,13 @@
 import react from '@astrojs/react'
 import { defineConfig } from 'astro/config'
+import { remarkModifiedTime } from './src/astro-plugins/remark-modified-time.mjs'
 
 import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), mdx()],
+  markdown: {
+    remarkPlugins: [remarkModifiedTime],
+  },
 })
