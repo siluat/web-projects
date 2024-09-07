578054672952d40213c64ee22cba90bed9b7f36b
siluat <siluat@gmail.com>
2024-09-07 20:07:27 +0900

    ⚡️ (dotori-note) 구글 태그 스크립트를 인라인으로 변경하고 partytown 설정

--- apps/dotori-note/astro.config.mjs
+++ apps/dotori-note/astro.config.mjs
@@ -1,12 +1,12 @@
+import mdx from '@astrojs/mdx'
+import partytown from '@astrojs/partytown'
 import react from '@astrojs/react'
 import { defineConfig } from 'astro/config'
 import { remarkModifiedTime } from './src/astro-plugins/remark-modified-time.mjs'
 
-import mdx from '@astrojs/mdx'
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx(), partytown()],
   markdown: {
     remarkPlugins: [remarkModifiedTime],
   },
