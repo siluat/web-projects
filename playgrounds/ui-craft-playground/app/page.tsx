ce1f0881bf5a89001e096a8523106d09777a1d26
siluat <siluat@gmail.com>
2025-03-05 17:17:51 +0900

    🔧 (ui-craft) Add UI Craft package and playground with initial component

---
+++ playgrounds/ui-craft-playground/app/page.tsx
@@ -0,0 +1,9 @@
+import { Foo } from '@siluat/ui-craft/foo';
+
+export default function Page() {
+  return (
+    <main className="flex flex-col items-center justify-center h-screen">
+      <Foo />
+    </main>
+  );
+}
