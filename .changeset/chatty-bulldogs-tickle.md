f349ab946c5ec9ce780a2afc75f5e93621497fea
siluat <siluat@gmail.com>
2025-03-15 13:23:20 +0900

    ✨ (ui-craft) Add FFXIV job icon transformation component

---
+++ .changeset/chatty-bulldogs-tickle.md
@@ -0,0 +1,5 @@
+---
+'@siluat/ui-craft': minor
+---
+
+Add FFXIV job icon transformation component
---
+++ packages/ui-craft/react/ffxiv-job-icon-transform-using-gooey-effect.test.tsx
@@ -0,0 +1,19 @@
+import { act, renderHook } from '@testing-library/react';
+import { useGooeyEffectClassName } from './ffxiv-job-icon-transform-using-gooey-effect.js';
+
+describe('useGooeyEffectClassName', () => {
+  it('should return the correct initial class names', () => {
+    const { result } = renderHook(() => useGooeyEffectClassName(3));
+    expect(result.current.classNames).toEqual(['showing', 'hidden', 'hidden']);
+  });
+
+  it('should return the correct class names after transit', () => {
+    const { result } = renderHook(() => useGooeyEffectClassName(3));
+
+    act(() => {
+      result.current.transit();
+    });
+
+    expect(result.current.classNames).toEqual(['hiding', 'showing', 'hidden']);
+  });
+});
