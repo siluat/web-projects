e0c2f1f7d7840e6ddc274e275f6beeaef4aa81de
Siluat <siluat@gmail.com>
2025-03-19 17:48:25 +0900

    💄 (ui-craft-playground) Add shadcn/ui sidebar (#12)

--- playgrounds/ui-craft-playground/app/page.tsx
+++ playgrounds/ui-craft-playground/app/page.tsx
@@ -1,6 +1,6 @@
 import { FfxivJobIconTransformUsingGooeyEffect } from '@siluat/ui-craft/react/ffxiv-job-icon-transform-using-gooey-effect';
 
-export default function Page() {
+export default function Page(): JSX.Element {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
       <span>FFXIV Job Icon Transform Using Gooey Effect</span>
