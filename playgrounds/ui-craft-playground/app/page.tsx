cc39c48e8887955bc762db028151b8a29ec7c709
Siluat <siluat@gmail.com>
2025-03-25 21:21:31 +0900

    ✨ (ui-craft) Add FFXIV job icons loop component using flubber (#13)
    * ✨ (ui-craft) Add FFXIV job icons loop component using flubber
    
    * ✏️  (ui-craft) update error message in useSvgPathMorphing

--- playgrounds/ui-craft-playground/app/page.tsx
+++ playgrounds/ui-craft-playground/app/page.tsx
@@ -1,10 +1,9 @@
-import { FfxivJobIconsLoopUsingGooeyEffect } from '@siluat/ui-craft/react/ffxiv-job-icons-loop-using-gooey-effect';
-
+import { FfxivJobIconsLoopUsingFlubber } from '@siluat/ui-craft/react/ffxiv-job-icons-loop-using-flubber';
 export default function Page() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
-      <span>FFXIV Job Icons Loop Using Gooey Effect</span>
-      <FfxivJobIconsLoopUsingGooeyEffect />
+      <span>FFXIV Job Icons Loop using Flubber</span>
+      <FfxivJobIconsLoopUsingFlubber />
     </main>
   );
 }
