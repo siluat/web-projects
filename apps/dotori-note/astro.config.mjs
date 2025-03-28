788d1c8ea3e1d8325ab003287f1590a993d59ef9
siluat <siluat@gmail.com>
2025-03-28 15:27:54 +0900

    🐛 (dotori-note): remove remarkModifiedTime plugin and update lastModified fields in content files

--- apps/dotori-note/astro.config.mjs
+++ apps/dotori-note/astro.config.mjs
@@ -2,12 +2,9 @@ import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
 import react from '@astrojs/react';
 import { defineConfig } from 'astro/config';
-import { remarkModifiedTime } from './src/astro-plugins/remark-modified-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), mdx(), partytown()],
-  markdown: {
-    remarkPlugins: [remarkModifiedTime],
-  },
+  markdown: {},
 });
