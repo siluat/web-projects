12cbff2b6986b9ba0dd642f2e98a8dafce32c1c9
siluat <siluat@gmail.com>
2025-03-30 18:59:29 +0900

    🏗️ (dotori-note): migrate styling to Tailwind CSS and remove Panda CSS related files

--- apps/dotori-note/astro.config.mjs
+++ apps/dotori-note/astro.config.mjs
@@ -1,10 +1,14 @@
 import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
 import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), mdx(), partytown()],
   markdown: {},
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
