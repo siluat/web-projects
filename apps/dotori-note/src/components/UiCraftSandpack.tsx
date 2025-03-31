15e20c6991b04c9e44e63958f3863de49cf05b22
siluat <siluat@gmail.com>
2025-03-31 18:32:17 +0900

    ✨(dotori-note): add components for ui craft log

---
+++ apps/dotori-note/src/components/UiCraftSandpack.tsx
@@ -0,0 +1,17 @@
+import {
+  SandpackCodeEditor,
+  SandpackLayout,
+  SandpackProvider,
+} from '@codesandbox/sandpack-react';
+
+export const UiCraftSandpack = (
+  props: React.ComponentProps<typeof SandpackProvider>,
+) => (
+  <SandpackProvider theme="light" {...props}>
+    <SandpackLayout
+      style={{ borderLeft: 'none', borderRight: 'none', borderRadius: 0 }}
+    >
+      <SandpackCodeEditor showLineNumbers />
+    </SandpackLayout>
+  </SandpackProvider>
+);
