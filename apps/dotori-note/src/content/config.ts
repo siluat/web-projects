5af3338770fb1c4138bb731eb28e4382d63832b6
siluat <siluat@gmail.com>
2025-04-01 17:32:13 +0900

    ♻️ (dotori-note): replace all content concepts with note

--- apps/dotori-note/src/content/config.ts
+++ apps/dotori-note/src/content/config.ts
@@ -1,16 +1,13 @@
 import { defineCollection, z } from 'astro:content';
 
-const contentScheme = z.object({
+const noteScheme = z.object({
   title: z.string(),
   description: z.string().optional(),
 });
 
 export const collections = {
-  /**
-   * 도토리 노트에 대한 기록
-   */
-  dotori: defineCollection({
+  note: defineCollection({
     type: 'content',
-    schema: contentScheme,
+    schema: noteScheme,
   }),
 };
