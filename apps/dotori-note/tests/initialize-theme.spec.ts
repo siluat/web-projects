60c0f886805762f0915e2a8e58a629e888df3387
siluat <siluat@gmail.com>
2025-04-14 16:41:12 +0900

    ✅ (dotori-note): rename test files

--- apps/dotori-note/tests/initialize-theme.spec.ts
+++ apps/dotori-note/tests/initialize-theme.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test.describe('Theme Tests', () => {
+test.describe('Initialize Theme', () => {
   test('should initialize with dark theme when system prefers dark mode', async ({
     page,
   }) => {
@@ -37,5 +37,15 @@ test.describe('Theme Tests', () => {
 
     await page.emulateMedia({ colorScheme: 'light' });
     await expect(page.locator('html')).not.toHaveClass(/dark/);
+  });
+});
+
+test.describe('Theme Setting', () => {
+  test('should update to dark theme when theme setting is dark', async ({
+    page,
+  }) => {
+    await page.goto('/');
+    const globalMenu = page.getByRole('button', { name: 'Global Menu' });
+    await expect(globalMenu).toBeVisible();
   });
 });
