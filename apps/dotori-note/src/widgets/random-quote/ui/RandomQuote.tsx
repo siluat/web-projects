e123eaf1ba8891e7520ff0ca9d32d2868cf715ad
siluat <siluat@gmail.com>
2025-04-20 22:24:15 +0900

    💄 (dotori-note): simplify RandomQuote component structure by removing unnecessary fragment

--- apps/dotori-note/src/widgets/random-quote/ui/RandomQuote.tsx
+++ apps/dotori-note/src/widgets/random-quote/ui/RandomQuote.tsx
@@ -7,24 +7,18 @@ const quote = pickRandomQuote(quotes);
 
 export function RandomQuote({ className }: React.ComponentProps<'figure'>) {
   return (
-    <>
-      {quote ? (
-        <figure
-          className={`${className} relative overflow-hidden bg-zinc-100 dark:bg-zinc-800 rounded-lg`}
-        >
-          <OpenQuote className="absolute -top-2 left-0 text-zinc-200 dark:text-zinc-600" />
-          <blockquote className="p-8 w-full text-center text-zinc-400 dark:text-zinc-400 text-sm">
-            {quote.original && <p className="original">{quote.original}</p>}
-            {quote.translated && (
-              <p className="translated">{quote.translated}</p>
-            )}
-            <footer>
-              <cite className="source">— {quote.source}</cite>
-            </footer>
-          </blockquote>
-          <CloseQuote className="absolute right-0 -bottom-2 text-zinc-200 dark:text-zinc-600" />
-        </figure>
-      ) : null}
-    </>
+    <figure
+      className={`${className} relative overflow-hidden bg-zinc-100 dark:bg-zinc-800 rounded-lg`}
+    >
+      <OpenQuote className="absolute -top-2 left-0 text-zinc-200 dark:text-zinc-600" />
+      <blockquote className="p-8 w-full text-center text-zinc-400 dark:text-zinc-400 text-sm">
+        {quote.original && <p className="original">{quote.original}</p>}
+        {quote.translated && <p className="translated">{quote.translated}</p>}
+        <footer>
+          <cite className="source">— {quote.source}</cite>
+        </footer>
+      </blockquote>
+      <CloseQuote className="absolute right-0 -bottom-2 text-zinc-200 dark:text-zinc-600" />
+    </figure>
   );
 }
