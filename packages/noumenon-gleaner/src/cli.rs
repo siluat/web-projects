44f62b22c9ea163c622e50cc21c840bd1adeeabf
siluat <siluat@gmail.com>
2025-06-27 22:34:02 +0900

    🚧 (noumenon-gleaner): update CLI to use PathBuf for input and output paths with added help descriptions

--- packages/noumenon-gleaner/src/cli.rs
+++ packages/noumenon-gleaner/src/cli.rs
@@ -1,12 +1,14 @@
+use std::path::PathBuf;
+
 use clap::Parser;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 pub struct Cli {
-    #[arg(short, long)]
-    pub input_file_path: String,
-    #[arg(short, long)]
-    pub output_dir_path: String,
+    #[arg(short, long, help = "입력 파일 경로")]
+    pub input_file_path: PathBuf,
+    #[arg(short, long, help = "출력 디렉토리 경로")]
+    pub output_dir_path: PathBuf,
 }
 
 #[test]
