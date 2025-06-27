46a097166e04567bb341234bb52a267474022ee0
Siluat <siluat@gmail.com>
2025-06-27 22:44:23 +0900

    🚧 (noumenon-gleaner): implement CLI for Noumenon Gleaner with input and output path arguments (#25)
    * 🚧 (noumenon-gleaner): implement CLI for Noumenon Gleaner with input and output path arguments
    
    * 🚧 (noumenon-gleaner): update CLI to use PathBuf for input and output paths with added help descriptions

--- packages/noumenon-gleaner/src/main.rs
+++ packages/noumenon-gleaner/src/main.rs
@@ -1,3 +1,15 @@
+mod cli;
+
+use clap::Parser;
+use cli::Cli;
+
 fn main() {
-    eprintln!("Gleaner is not implemented yet");
+    let cli = Cli::parse();
+
+    println!("input_file_path: {:?}", cli.input_file_path);
+    println!("output_dir_path: {:?}", cli.output_dir_path);
+
+    // TODO: Read input file
+    // TODO: Extract schema and data from input file
+    // TODO: Write schema and data to output directory
 }
